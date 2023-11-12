27c4aeafc2fea199133f6efb660a5a6783529f2e
ArcAnya <arcanyatwitch@gmail.com>
2023-11-12 07:24:10 +0100

    add charity + text

--- dApp/frontend/components/CharityDonation.tsx
+++ dApp/frontend/components/CharityDonation.tsx
@@ -7,11 +7,29 @@ import {
     ModalFooter,
     ModalBody,
     ModalCloseButton,
-    useDisclosure
+    useDisclosure,
+    Box,
+    Text
   } from '@chakra-ui/react'
+  import Image from 'next/image';
+  import { useState } from 'react';
+
+  const descriptionCSS = {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    margin: '2rem',
+    backgroundColor: 'red',
+  }
 
   export function CharityDonation() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [openDescription, setOpenDescription] = useState(false);
+    const selectCharity = () => {
+      setOpenDescription(!openDescription)
+    }
+
     return (
       <>
         <Button onClick={onOpen}>Deposit</Button>
@@ -19,17 +37,29 @@ import {
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
+            <ModalHeader>Support a Charity</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              Would you like to donate to one of the following charities?
+              <p>
+              Would you like to donate to a charity?
+              </p>
+              <button onClick={selectCharity}>
+                <Image src='/../public/TGBLogo.png' alt='The Giving Block Logo' width={200} height={200} />
+              </button>
+              {openDescription && (
+                <Box>
+                  <Text fontSize="sm">
+                    The Giving Block is a registered 501(c)3 nonprofit organization headquartered in Washington, D.C. that is transforming philanthropy by providing solutions that make giving crypto tax efficient. The Giving Block equips nonprofits to fundraise cryptocurrencies like Bitcoin, providing education, training and a technical solution. For donors, they built the only cryptocurrency donation platform designed specifically for tax optimization. They have partnered with over 250 nonprofits and raised millions in cryptocurrency. The Giving Block’s goal is to make accepting cryptocurrency donations just as easy as credit card donations.
+                    </Text>
+                    </Box>
+                    )}
             </ModalBody>
   
             <ModalFooter>
               <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
+                Donate
               </Button>
-              <Button variant='ghost'>Secondary Action</Button>
+              <Button variant='ghost'>No, I don&apos;t want to donate</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
