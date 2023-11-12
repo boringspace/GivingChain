1998d932e0c9acaa3d4256288d52644ce0045e69
ArcAnya <arcanyatwitch@gmail.com>
2023-11-12 09:05:19 +0100

    add input for amount & some design modifs!

--- dApp/frontend/components/CharityDonation.tsx
+++ dApp/frontend/components/CharityDonation.tsx
@@ -9,25 +9,30 @@ import {
     ModalCloseButton,
     useDisclosure,
     Box,
-    Text
+    Text,
+    Input
   } from '@chakra-ui/react'
   import Image from 'next/image';
-  import { useState } from 'react';
-
-  const descriptionCSS = {
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-    color: 'black',
-    textAlign: 'center',
-    margin: '2rem',
-    backgroundColor: 'red',
-  }
+  import { useState } from 'react';  
 
   export function CharityDonation() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [openDescription, setOpenDescription] = useState(false);
+    const [amount, setAmount] = useState<string>("");
+    const [isLoading, setLoading] = useState(false);
+
+    const isAmountValid = (amount: string) => {
+      const validShareRegEx = /^\d{1,3}(\.\d{0,6})?$/;
+      return validShareRegEx.test(amount);
+    };
+
     const selectCharity = () => {
       setOpenDescription(!openDescription)
+    }
+
+    const descriptionCSS = {
+      border: `${openDescription ? '4px solid #ccc' : 'none'}`,
+      borderRadius: `${openDescription ? '10px' : 'none'}`,
     }
 
     return (
@@ -40,26 +45,34 @@ import {
             <ModalHeader>Support a Charity</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <p>
+              <Text pb={4}>
               Would you like to donate to a charity?
-              </p>
-              <button onClick={selectCharity}>
-                <Image src='/../public/TGBLogo.png' alt='The Giving Block Logo' width={200} height={200} />
+              </Text>
+              <Input 
+                value={amount} onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setAmount(event.currentTarget.value)} 
+                placeholder='Enter token amount' 
+                mb={4} 
+                isInvalid={!isAmountValid(amount)}
+                />
+              <button onClick={selectCharity} >
+                <Image src='/../public/TGBLogo.png' alt='The Giving Block Logo' width={200} height={200} style={descriptionCSS}/>
               </button>
               {openDescription && (
-                <Box>
+                <Box mt={2}>
                   <Text fontSize="sm">
                     The Giving Block is a registered 501(c)3 nonprofit organization headquartered in Washington, D.C. that is transforming philanthropy by providing solutions that make giving crypto tax efficient. The Giving Block equips nonprofits to fundraise cryptocurrencies like Bitcoin, providing education, training and a technical solution. For donors, they built the only cryptocurrency donation platform designed specifically for tax optimization. They have partnered with over 250 nonprofits and raised millions in cryptocurrency. The Giving Block’s goal is to make accepting cryptocurrency donations just as easy as credit card donations.
                     </Text>
                     </Box>
                     )}
+               
             </ModalBody>
   
             <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={onClose}>
+              <Button isLoading={isLoading} type="submit" mr={3} onClick={onClose}>
                 Donate
               </Button>
-              <Button variant='ghost'>No, I don&apos;t want to donate</Button>
+              <Button variant='ghost' mr={3} onClick={onClose}>No, I don&apos;t want to donate</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
